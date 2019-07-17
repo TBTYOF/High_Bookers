6192dc97798501ccdcd593ed224a60b596e28462
TBATYOF <toribatyof@gmail.com>
2019-07-17 20:31:08 +0900

    [Add]マイページの形だけ完成

--- Bookers/app/controllers/users_controller.rb
+++ Bookers/app/controllers/users_controller.rb
@@ -6,10 +6,16 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
+		@book = Book.new
+		@books = Book.all
+	end
+
+	def edit
+		@user = User.find(params[:id])
 	end
 
 	private
 	def user_params
-		params.require(:user).permit(:name, :introduction, :profile_image_id)
+		params.require(:user).permit(:name, :introduction, :profile_image)
 	end
 end
